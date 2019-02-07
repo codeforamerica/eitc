0be43a7c30fab19be92b3c0dd597ab0bc09e8d84
Ben Golder <bgolder@codeforamerica.org>
2019-02-07 10:51:32 -0800

    Add to default mixpanel properties

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_visitor_id
+  before_action :set_source_in_session
   before_action :track_page_view, only: [:show, :edit, :new]
 
   def set_visitor_id
@@ -11,6 +12,20 @@ class ApplicationController < ActionController::Base
     session[:visitor_id]
   end
 
+  def source
+    session[:source]
+  end
+
+  def set_source_in_session
+    if params[:source]
+      session[:source] = params[:source]
+    elsif params[:s]
+      session[:source] = params[:s]
+    elsif request.headers.fetch(:referer, "").include?("google.com")
+      session[:source] = "organic_google"
+    end
+  end
+
   def user_agent
     @user_agent ||= DeviceDetector.new(request.user_agent)
   end
@@ -20,20 +35,24 @@ class ApplicationController < ActionController::Base
   end
 
   def send_mixpanel_event(event_name:, data: {})
+    major_browser_version = user_agent.full_version.try { |v| v.partition('.').first }
     unless user_agent.bot?
       default_data = {
+          source: source,
+          referrer: request.referrer,
           full_user_agent: request.user_agent,
           browser_name: user_agent.name,
           browser_full_version: user_agent.full_version,
-          browser_major_version: user_agent.full_version.try { |version| version.partition('.').first },
+          browser_major_version: major_browser_version,
           os_name: user_agent.os_name,
           os_full_version: user_agent.os_full_version,
-          os_major_version: user_agent.os_full_version.try { |version| version.partition('.').first },
+          os_major_version: user_agent.os_full_version.try { |v| v.partition('.').first },
           is_bot: user_agent.bot?,
           bot_name: user_agent.bot_name,
           device_brand: user_agent.device_brand,
           device_name: user_agent.device_name,
           device_type: user_agent.device_type,
+          device_browser_version: "#{user_agent.os_name} #{user_agent.device_type} #{user_agent.name} #{major_browser_version}",
           locale: I18n.locale,
           path: request.path,
           full_path: request.fullpath,
