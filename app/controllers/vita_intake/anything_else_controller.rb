18a63d7c10a16bfc5b962585c356dae319a88b23
loumoore <lourmoore@gmail.com>
2019-05-09 00:04:08 -0700

    Updated front service to send all client, spouse, and dependent data to Front in the body of the custom intake message. Also updated the add dependents form to use a date selector.

--- app/controllers/vita_intake/anything_else_controller.rb
+++ app/controllers/vita_intake/anything_else_controller.rb
@@ -4,5 +4,23 @@ module VitaIntake
     def form_class
       AnythingElseForm
     end
+
+    def update
+      @form = form_class.new(current_record, form_params)
+      if @form.valid?
+        @form.save
+        update_session
+        track_form_progress
+        send_application_to_front
+        redirect_to(next_path)
+      else
+        track_validation_errors
+        render :edit
+      end
+    end
+
+    def send_application_to_front
+      FrontService.instance.send_client_application(current_record)
+    end
   end
 end
--- app/forms/add_dependent_form.rb
+++ app/forms/add_dependent_form.rb
@@ -1,11 +1,25 @@
 class AddDependentForm < Form
-  set_attributes_for :household_member, :first_name, :last_name, :birthdate, :full_time_student, :non_citizen, :disabled, :legally_blind
+  set_attributes_for :household_member, :first_name, :last_name, :birthdate_year, :birthdate_month,
+                     :birthdate_day, :full_time_student, :non_citizen, :disabled, :legally_blind
 
   validates_presence_of :first_name, message: "Make sure to provide a first name."
   validates_presence_of :last_name, message: "Make sure to provide a last name."
-  validates_presence_of :birthdate, message: "Make sure to provide a date of birth."
+  attr_internal_reader :birthdate
+  validates :birthdate, date: true
 
   def save
-    record.update(attributes_for(:household_member))
+    record.update(household_member_attributes)
+  end
+
+  private
+
+  def household_member_attributes
+    attributes = attributes_for(:household_member)
+    attributes[:birthdate] = to_datetime(birthdate_year, birthdate_month, birthdate_day)
+    attributes.except(
+        :birthdate_year,
+        :birthdate_month,
+        :birthdate_day,
+        )
   end
 end
