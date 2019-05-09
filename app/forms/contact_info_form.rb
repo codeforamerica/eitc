2b9f51efb57be68ec346d1d87f6c139c458ca058
Ben Golder <bgolder@codeforamerica.org>
2019-05-09 15:15:34 -0700

    Stronger validation for phone numbers

--- app/forms/contact_info_form.rb
+++ app/forms/contact_info_form.rb
@@ -2,6 +2,7 @@ class ContactInfoForm < Form
   set_attributes_for :vita_client, :phone_number, :email, :sms_enabled
 
   validates_presence_of :phone_number, message: "Make sure to provide a phone number."
+  validates :phone_number, phone: { message: "Make sure to provide a valid phone number." }, allow_blank: true
   validates_presence_of :email, message: "Make sure to provide an email."
 
   def save
