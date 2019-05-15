2414abdb36c8e36871b8e7a085099fca5517908c
loumoore <lourmoore@gmail.com>
2019-05-14 18:38:40 -0700

    copy updates and bug fixes related to the boolean values in household_members and vita_clients

--- app/models/household_member.rb
+++ app/models/household_member.rb
@@ -5,6 +5,22 @@ class HouseholdMember < ApplicationRecord
 
   validates_inclusion_of :relation, :in => RELATION_CHOICES
 
+  def full_time_student?
+    full_time_student == '1'
+  end
+
+  def non_citizen?
+    non_citizen == '1'
+  end
+
+  def disabled?
+    disabled == '1'
+  end
+
+  def legally_blind?
+    legally_blind == '1'
+  end
+
   def analytics_data
     {}
   end
