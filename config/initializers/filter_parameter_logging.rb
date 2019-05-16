57199473cb83301efc378516296c8b20da257023
Ben Golder <bgolder@codeforamerica.org>
2019-05-15 19:10:05 -0700

    filter sensitive params from logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+    :password, :last_four_ssn, :last_four_ssn_spouse,
+    :email, :phone_number, :non_citizen, :disabled, :legally_blind,
+    :full_time_student, :birthdate, :birthdate_year, :birthdate_month,
+    :birthdate_day, :first_name, :last_name, :street_address, :zip, :city,
+    :signature, :spouse_signature, :anything_else
+]
