70f51a90f73234b94abac75cd5978733a36fa335
loumoore <lourmoore@gmail.com>
2019-05-22 01:19:01 -0700

    flash messages when sending a signing request

--- app/controllers/signing_request_controller.rb
+++ app/controllers/signing_request_controller.rb
@@ -11,12 +11,19 @@ class SigningRequestController < ApplicationController
       @vita_client = VitaClient.find_by_id(params[:vita_client_id])
       @signature_document = params[:signature_document]
       @prepared_return = params[:prepared_return]
-      signing_request = SigningRequest.new(vita_client_id: @vita_client.id, email: @vita_client.email, unique_key: self.class.generate_unique_key)
-      signing_request.signature_document.attach(@signature_document)
-      signing_request.prepared_return.attach(@prepared_return)
-      signing_request.save
+      @signing_request = SigningRequest.new(vita_client_id: @vita_client.id, email: @vita_client.email, unique_key: self.class.generate_unique_key)
+      @signing_request.signature_document.attach(@signature_document)
+      @signing_request.prepared_return.attach(@prepared_return)
+      @signing_request.save
 
-      EmailSigningRequestToClientJob.perform_later(email: @vita_client.email, link: signing_request.signature_url )
+      EmailSigningRequestToClientJob.perform_later(email: @vita_client.email, link: @signing_request.signature_url )
+      if @signature_document.present? && @prepared_return.present?
+        flash[:success] = "A request for e-file authorization signature has been sent to #{@vita_client.email}"
+        redirect_to signing_request_path
+      else
+        flash[:error] = "You must attach both a Signature Document and a Prepared Return"
+        render "index"
+      end
     end
   end
 
